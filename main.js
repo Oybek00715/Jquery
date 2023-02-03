cf236afb025e3e4955a52dd783a14aa0b7feba94
unknown <beljan.beka123.123@gmail.com>
2023-02-03 15:31:40 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,74 @@
+// hide()   dispaly - none
+// snow()    display - block
+// fadeIn()  display - block
+// fadeOut() display - none
+// slideDown() display - block
+// slideUp() dispaly - none
+// slideToggle() display == block =>display=none
+        //display == none => display = block   
+
+
+
+// $(".block").fadeIn(4000)
+// $(".block").fadeOut(5000)
+
+// $(".block").slideDown(5000)
+// $(".block").slideUp(5000)
+
+// $(".block").slideToggle(10000)
+
+
+
+$('#btn').click(()=>{
+
+
+
+// $(".box1").show(1000)
+// $(".box2").show(2000)   
+// $(".box3").show(3000)
+// $(".box4").show(4000)
+// $(".box5").show(5000)
+
+
+// $(".box1").hide(6000)
+// $(".box2").hide(7000)
+// $(".box3").hide(8000)
+// $(".box4").hide(9000)
+// $(".box5").hide(10000)
+
+
+// $(".box1").fadeIn(1000)
+// $(".box2").fadeIn(2000)   
+// $(".box3").fadeIn(3000)
+// $(".box4").fadeIn(4000)
+// $(".box5").fadeIn(5000)
+
+
+// $(".box1").fadeOut(6000)
+// $(".box2").fadeOut(7000)
+// $(".box3").fadeOut(8000)
+// $(".box4").fadeOut(9000)
+// $(".box5").fadeOut(10000)
+
+// $(".box1").slideDown(1000)
+// $(".box2").slideDown(2000)   
+// $(".box3").slideDown(3000)
+// $(".box4").slideDown(4000)
+// $(".box5").slideDown(5000)
+
+
+// $(".box1").slideUp(6000)
+// $(".box2").slideUp(7000)
+// $(".box3").slideUp(8000)
+// $(".box4").slideUp(9000)
+// $(".box5").slideUp(10000)
+
+$(".box1").slideToggle(1000)
+$(".box2").slideToggle(2000)   
+$(".box3").slideToggle(3000)
+$(".box4").slideToggle(4000)
+$(".box5").slideToggle(5000)
+
+
+
+})
